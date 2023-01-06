7bdfbd088eb42383dc78f81dd191bba5db925738
lulu <sarah.j.ting@gmail.com>
2023-01-06 14:34:13 +0900

    ui: add login links to the topbar

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -14,9 +14,26 @@
         @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     </head>
     <body>
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <nav x-data="{ open: false }" class="navbar navbar-expand-md bg-body-tertiary">
             <div class="container-fluid">
                 <a class="navbar-brand" href="/">{{ __("RetailAI Homework") }}</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <x-nav-link :href="route('login', ['role' => App\Enums\RoleEnum::MERCHANT->value])">
+                                {{ __('Merchant login') }}
+                            </x-nav-link>
+                        </li>
+                        <li class="nav-item">
+                            <x-nav-link :href="route('login', ['role' => App\Enums\RoleEnum::ADMIN->value])">
+                                {{ __('Admin login') }}
+                            </x-nav-link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
 
